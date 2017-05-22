c20068b79cf5a9ee89c5130a38cfd15a64ab3828
marcuschoong <marcuschoong@starsystems.biz>
2017-05-22 09:29:50 +0800

    update login credential
    update key to tenant1

--- AndroidStudio/src/main/java/com/starsystems/myapplication/MainActivity.java
+++ AndroidStudio/src/main/java/com/starsystems/myapplication/MainActivity.java
@@ -12,11 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Talkm DEV
-//        TalkMSDKConfig.initTalkM(MainActivity.this, "tenant1", "ax7KcAbAvfgXxzeA", "PS3Y5YlOpdSsFsoEydq7ahTYRxOYvicgKnGF23cZgf5HimmMQXTUhW4djx0IwQ5v");
-
         //talkm uat
-        TalkMSDKConfig.initTalkM(MainActivity.this, "starsystems", "de201968-ccc6-4e95-a14f-e1e470b1f610", "vvLvRcm2eKF9A8ILTEPVnLl2k3DtXJKe7HQlXGQT49HGYbtul1kZWdoVFj4g3olk");
+        TalkMSDKConfig.initTalkM(MainActivity.this, "tenant1", "hrXXPdGknlGLYvbi", "SSfEsfil0CmKeDsyDJwRHVwXXCKAdpqY9pTuO0r6arF6QTSb2jbu8WOBNPk9rFcW");
 
     }
 }
